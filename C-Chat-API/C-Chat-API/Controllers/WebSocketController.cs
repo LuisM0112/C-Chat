c0650bfddba036eb4da6d2134306a79ac96caf00
LuisM0112 <luismigase@gmail.com>
2024-04-30 14:46:44 +0200

    Create WebSocketController.cs

---
+++ C-Chat-API/C-Chat-API/Controllers/WebSocketController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.WebSockets;
+
+namespace C_Chat_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WebSocketController : ControllerBase
+    {
+        [Route("/ws")]
+        public async Task Get()
+        {
+            if (HttpContext.WebSockets.IsWebSocketRequest)
+            {
+                using WebSocket webSocket = await HttpContext.WebSockets.AcceptWebSocketAsync();
+                await ProcessWebSocket(webSocket);
+            }
+            else
+            {
+                HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+            }
+        }
+
+        private async Task ProcessWebSocket(WebSocket webSocket)
+        {
+            byte[] buffer = new byte[1024 * 4];
+            WebSocketReceiveResult receiveResult;
+
+            do
+            {
+                receiveResult = await webSocket.ReceiveAsync(buffer, CancellationToken.None);
+
+                await webSocket.SendAsync(
+                    new ArraySegment<byte>(buffer, 0, receiveResult.Count),
+                    receiveResult.MessageType,
+                    receiveResult.EndOfMessage,
+                    CancellationToken.None);
+            }
+            while (!receiveResult.CloseStatus.HasValue);
+
+            await webSocket.CloseAsync(
+                receiveResult.CloseStatus.Value,
+                receiveResult.CloseStatusDescription,
+                CancellationToken.None);
+        }
+    }
+}

d3fcb5904eaa959f312cfd5766d1df43010b9cf7
LuisM0112 <luismigase@gmail.com>
2024-05-28 16:56:33 +0200

    Removed not used files

--- C-Chat-Web/src/app/model/classes/user-chat-insert.spec.ts
+++
@@ -1,7 +0,0 @@
-import { UserChatInsert } from './user-chat-insert';
-
-describe('UserChatInsert', () => {
-  it('should create an instance', () => {
-    expect(new UserChatInsert()).toBeTruthy();
-  });
-});
